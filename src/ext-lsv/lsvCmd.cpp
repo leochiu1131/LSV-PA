f066dbdd2fb21055575b43c13c112b4b6869a2f8
wilson22510120 <wilson22510120@gmail.com>
2023-09-20 16:55:03 +0800

    simbdd command

--- src/ext-lsv/lsvCmd.cpp
+++ src/ext-lsv/lsvCmd.cpp
@@ -1,11 +1,16 @@
 #include "base/abc/abc.h"
 #include "base/main/main.h"
 #include "base/main/mainInt.h"
+#include <iostream>
 
 static int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv);
+static int Lsv_CommandSimBdd(Abc_Frame_t* pAbc, int argc, char** argv);
+// static int Lsv_CommandSimAig(Abc_Frame_t* pAbc, int argc, char** argv);
 
 void init(Abc_Frame_t* pAbc) {
   Cmd_CommandAdd(pAbc, "LSV", "lsv_print_nodes", Lsv_CommandPrintNodes, 0);
+  Cmd_CommandAdd(pAbc, "LSV", "lsv_sim_bdd", Lsv_CommandSimBdd, 0);
+  // Cmd_CommandAdd(pAbc, "LSV", "lsv_sim_aig", Lsv_CommandSimAig, 0);
 }
 
 void destroy(Abc_Frame_t* pAbc) {}
@@ -55,6 +60,48 @@ int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv) {
 usage:
   Abc_Print(-2, "usage: lsv_print_nodes [-h]\n");
   Abc_Print(-2, "\t        prints the nodes in the network\n");
+  Abc_Print(-2, "\t-h    : print the command usage\n");
+  return 1;
+}
+
+void Lsv_NtkSimBdd(Abc_Ntk_t* pNtk, char* arg) {
+
+}
+
+
+int Lsv_CommandSimBdd(Abc_Frame_t* pAbc, int argc, char** argv) {
+  Abc_Ntk_t* pNtk = Abc_FrameReadNtk(pAbc);
+  std::cout << "argc: " << argc << std::endl;
+  std::cout << "argv0: " << argv[0] << std::endl;
+  std::cout << "argv1: " << argv[1] << std::endl;
+  int c;
+  Extra_UtilGetoptReset();
+  while ((c = Extra_UtilGetopt(argc, argv, "h")) != EOF) {
+    switch (c) {
+      case 'h':
+        goto usage;
+      default:
+        goto usage;
+    }
+  }
+  if (!pNtk) {
+    Abc_Print(-1, "Empty network.\n");
+    return 1;
+  }
+  if (!Abc_NtkIsBddLogic(pNtk)) {
+    Abc_Print(-1, "Network is not logic BDD networks (run \"collapse\").\n");
+    return 1;
+  }
+  // if () {
+  //   Abc_Print(-1, "Network is not logic BDD networks (run \"collapse\").\n");
+  //   return 1;
+  // }
+  Lsv_NtkSimBdd(pNtk, argv[1]);
+  return 0;
+
+usage:
+  Abc_Print(-2, "usage: lsv_sim_bdd [-h]\n");
+  Abc_Print(-2, "\t        simulate the Ntk with bdd\n");
   Abc_Print(-2, "\t-h    : print the command usage\n");
   return 1;
 }
