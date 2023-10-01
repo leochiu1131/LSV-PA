398254e13640ed6880c1aa1cc0b7cba35563b226
2a-GITHUB <58847449+2a-GITHUB@users.noreply.github.com>
2023-10-01 22:08:52 +0800

    update sim bdd

---
+++ src/ext-lsv/simBdd/bdd.h
@@ -0,0 +1,11 @@
+#ifndef SIMBDD_h
+#define SIMBDD_h
+
+#include "base/abc/abc.h"
+
+ABC_NAMESPACE_HEADER_START
+
+extern int Abc_NtkSimBdd(Abc_Ntk_t * pNtk, char* piValue);
+
+#endif
+ABC_NAMESPACE_HEADER_END
