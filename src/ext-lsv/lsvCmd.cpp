2152e9d87f8f73d85eaf66086fbb67f927bff674
b09901047 <b09901047@g.ntu.edu.tw>
2023-11-18 11:45:46 +0800

    complete lsv_sym_sat and revise lsv_sym_bdd

--- src/ext-lsv/lsvCmd.cpp
+++ src/ext-lsv/lsvCmd.cpp
@@ -476,50 +476,69 @@ void Lsv_NtkSymBdd(Abc_Ntk_t* pNtk, char* output, char* input1, char* input2) {
   Abc_Obj_t* Po = Abc_NtkPo(pNtk, k);
   string pattern1, pattern2;
   Abc_Obj_t* func = Abc_ObjFanin0(Po); //the original function
+  char* input1_name = Abc_ObjName(Abc_NtkPi(pNtk, i));
+  char* input2_name = Abc_ObjName(Abc_NtkPi(pNtk, j));
+  char** pNamesIn = (char**)Abc_NodeGetFaninNames(func)->pArray;
+  int FaninNum = Abc_ObjFaninNum(func);
+  int n = Abc_NtkCiNum(pNtk);
   DdNode* temp1 = (DdNode*) func->pData;
-  Cudd_Ref(temp1);
-  DdNode* temp1_1 = Cudd_Cofactor( dd, temp1, Cudd_Not(dd->vars[i]) );
-  Cudd_Ref(temp1_1);
-  DdNode* temp1_2 = Cudd_Cofactor( dd, temp1_1, dd->vars[j] );
-  Cudd_Ref(temp1_2);
   DdNode* temp2 = (DdNode*) func->pData;
+  Cudd_Ref(temp1);
   Cudd_Ref(temp2);
-  DdNode* temp2_1 = Cudd_Cofactor( dd, temp2, dd->vars[i] );
-  Cudd_Ref(temp2_1);
-  DdNode* temp2_2 = Cudd_Cofactor( dd, temp2_1, Cudd_Not(dd->vars[j]) );
-  Cudd_Ref(temp2_2);
-  Cudd_RecursiveDeref(dd, temp1);
-  Cudd_RecursiveDeref(dd, temp1_1);
-  Cudd_RecursiveDeref(dd, temp2);
-  Cudd_RecursiveDeref(dd, temp2_1);
-  if (temp1_2 == temp2_2) {
+  for(int a = 0; a < FaninNum; a++){
+    if(strcmp(pNamesIn[a], input1_name) == 0){
+      DdNode* temp1_1 = Cudd_Cofactor( dd, temp1, Cudd_Not(dd->vars[a]));
+      Cudd_RecursiveDeref(dd, temp1);
+      Cudd_Ref(temp1_1);
+      temp1 = temp1_1;
+
+      DdNode* temp2_1 = Cudd_Cofactor( dd, temp2, dd->vars[a]);
+      Cudd_RecursiveDeref(dd, temp2);
+      Cudd_Ref(temp2_1);
+      temp2 = temp2_1;
+    }
+    if(strcmp(pNamesIn[a], input2_name) == 0){
+      DdNode* temp1_1 = Cudd_Cofactor( dd, temp1, dd->vars[a]);
+      Cudd_RecursiveDeref(dd, temp1);
+      Cudd_Ref(temp1_1);
+      temp1 = temp1_1;
+
+      DdNode* temp2_1 = Cudd_Cofactor( dd, temp2, Cudd_Not(dd->vars[a]));
+      Cudd_RecursiveDeref(dd, temp2);
+      Cudd_Ref(temp2_1);
+      temp2 = temp2_1;
+    }
+  }
+
+  if (temp1 == temp2) {
+  // if (Cudd_EqualSupNorm(dd, temp1_2, temp2_2, 0, 0)) {
     printf("symmetric\n");
-    Cudd_RecursiveDeref(dd, temp1_2);
-    Cudd_RecursiveDeref(dd, temp2_2);
+    Cudd_RecursiveDeref(dd, temp1);
+    Cudd_RecursiveDeref(dd, temp2);
   }
   else {
     // Recur_get_diff_pattern(dd, pattern1, pattern2, 0, Abc_NtkCiNum(pNtk), temp1_2, temp2_2, i, j);
-    for (int n = 0; n < Abc_NtkCiNum(pNtk); n++) {
-      if (n == i) {
+    for (n = 0; n < FaninNum; n++) {
+      if(strcmp(pNamesIn[n], input1_name) == 0){
         pattern1 += "0";
         pattern2 += "1";
       }
-      else if (n == j) {
+      else if(strcmp(pNamesIn[n], input2_name) == 0){
         pattern1 += "1";
         pattern2 += "0";
       }
       else {
-        temp1_3 = temp1_2;
-        temp2_3 = temp2_2;
-        temp1_2 = Cudd_Cofactor( dd, temp1_3, dd->vars[n] );
-        temp2_2 = Cudd_Cofactor( dd, temp2_3, dd->vars[n] );
-        Cudd_Ref(temp1_2);
-        Cudd_Ref(temp2_2);
-        if (temp1_2 == temp2_2) {
-          Cudd_RecursiveDeref(dd, temp1_2);
-          Cudd_RecursiveDeref(dd, temp2_2);
-          temp1_2 = Cudd_Cofactor( dd, temp1_3, Cudd_Not(dd->vars[n]) );
-          temp2_2 = Cudd_Cofactor( dd, temp2_3, Cudd_Not(dd->vars[n]) );
+        temp1_3 = temp1;
+        temp2_3 = temp2;
+        temp1 = Cudd_Cofactor( dd, temp1_3, dd->vars[n] );
+        temp2 = Cudd_Cofactor( dd, temp2_3, dd->vars[n] );
+        Cudd_Ref(temp1);
+        Cudd_Ref(temp2);
+        if (temp1 == temp2) {
+          Cudd_RecursiveDeref(dd, temp1);
+          Cudd_RecursiveDeref(dd, temp2);
+          temp1 = Cudd_Cofactor( dd, temp1_3, Cudd_Not(dd->vars[n]) );
+          temp2 = Cudd_Cofactor( dd, temp2_3, Cudd_Not(dd->vars[n]) );
           pattern1 += "0";
           pattern2 += "0";
         }
@@ -565,25 +584,106 @@ usage:
 
 
 void Lsv_NtkSymSat(Abc_Ntk_t* pNtk, char* output, char* input1, char* input2) {
-  // int k = stoi(output);
-  // int i = stoi(input1);
-  // int j = stoi(input2);
-  // int m;
-  // Abc_Obj_t* Po = Abc_NtkPo(pNtk, k);
-  // Abc_Ntk_t* output_cone = Abc_NtkCreateCone(pNtk, Po, Abc_ObjName(Po), 0);
-  // Aig_Man_t* aig_1 = Abc_NtkToDar(output_cone, 0, 0);
-  // sat_solver* solver = sat_solver_new();
-  // Cnf_Dat_t* cnf_1 =  Cnf_Derive(aig_1, 1);
-  // solver = (sat_solver*)Cnf_DataWriteIntoSolverInt(solver, cnf_1, 1, 0);
-  // Cnf_Dat_t* cnf_2 =  Cnf_DataDup(cnf_1);
-  // Cnf_DataLift(cnf_2, cnf_2->nVars * 3);
+  int k = stoi(output);
+  int i = stoi(input1);
+  int j = stoi(input2);
+  int m;
+  Abc_Obj_t* Po = Abc_NtkPo(pNtk, k);
+  Abc_Obj_t* Po_node = Abc_ObjFanin0(Po);
+  Abc_Ntk_t* output_cone = Abc_NtkCreateCone(pNtk, Po_node, Abc_ObjName(Po), 1);
+  Aig_Man_t* aig_1 = Abc_NtkToDar(output_cone, 0, 0);
+  sat_solver* solver = sat_solver_new();
+  Cnf_Dat_t* cnf_1 =  Cnf_Derive(aig_1, 1);
+  Cnf_Dat_t* cnf_2 =  Cnf_Derive(aig_1, 1);
+  Cnf_DataLift(cnf_2, cnf_2->nVars);
+  printf("vars1: %d\n", cnf_1->nVars);
+  // printf("vars2: %d\n", cnf_2->nVars);
+  solver = (sat_solver*)Cnf_DataWriteIntoSolverInt(solver, cnf_1, 1, 0);
+  solver = (sat_solver*)Cnf_DataWriteIntoSolverInt(solver, cnf_2, 1, 0);
   // solver = (sat_solver*)Cnf_DataWriteIntoSolverInt(solver, cnf_2, 1, 0);
-  // Abc_Obj_t* Pi = Abc_NtkPi(pNtk, i);
-  // Abc_Obj_t* Pj = Abc_NtkPi(pNtk, j);
-  // int var_1_i = cnf_1->pVarNums[Pi->Id];
-  // int var_1_j = cnf_1->pVarNums[Pj->Id];
+
+  // Aig_Obj_t* Pi = Aig_ManCi(aig_1, i);
+  // Aig_Obj_t* Pj = Aig_ManCi(aig_1, j);
+  // Aig_Obj_t* Pk = Aig_ManCo(aig_1, k);
+  // int var_1_out = cnf_2->pVarNums[Pk->Id];
+  // int var_1_i = cnf_2->pVarNums[Pi->Id];
+  // int var_1_j = cnf_2->pVarNums[Pj->Id];
+
+  // Abc_Obj_t* Pi;
+  // Abc_Obj_t* Pj;
+  // Abc_Obj_t* Pk;
+  Aig_Obj_t* Pi;
+  Aig_Obj_t* Pj;
+  Aig_Obj_t* Pk;
+  Aig_ManForEachCi(aig_1, Pi, m) {
+  // Abc_NtkForEachCi(pNtk, Pi, m) {
+    if (m != i && m != j) {
+      int array[] = {toLitCond(cnf_1->pVarNums[Pi->Id], 1), toLitCond(cnf_2->pVarNums[Pi->Id], 0)};
+      int array1[] = {toLitCond(cnf_1->pVarNums[Pi->Id], 0), toLitCond(cnf_2->pVarNums[Pi->Id], 1)};
+      sat_solver_addclause(solver, array, array + sizeof(array) / sizeof(int));
+      sat_solver_addclause(solver, array1, array1 + sizeof(array1) / sizeof(int));
+    }
+  }
+  Pi = Aig_ManCi(aig_1, i);
+  Pj = Aig_ManCi(aig_1, j);
+  Pk = Aig_ManCo(aig_1, 0);
+  // Pi = Abc_NtkCi(pNtk, i);
+  // Pj = Abc_NtkCi(pNtk, j);
+  // Pk = Abc_NtkCo(pNtk, k);
+  
+  int array[] = {toLitCond(cnf_1->pVarNums[Pi->Id], 1), toLitCond(cnf_2->pVarNums[Pj->Id], 0)};
+  int array1[] = {toLitCond(cnf_1->pVarNums[Pi->Id], 0), toLitCond(cnf_2->pVarNums[Pj->Id], 1)};
+  sat_solver_addclause(solver, array, array + sizeof(array) / sizeof(int));
+  sat_solver_addclause(solver, array1, array1 + sizeof(array1) / sizeof(int));
+  int array2[] = {toLitCond(cnf_1->pVarNums[Pj->Id], 1), toLitCond(cnf_2->pVarNums[Pi->Id], 0)};
+  int array3[] = {toLitCond(cnf_1->pVarNums[Pj->Id], 0), toLitCond(cnf_2->pVarNums[Pi->Id], 1)};
+  sat_solver_addclause(solver, array2, array2 + sizeof(array2) / sizeof(int));
+  sat_solver_addclause(solver, array3, array3 + sizeof(array3) / sizeof(int));
+  int miter = sat_solver_addvar(solver);
+  int array4[] = {toLitCond(cnf_1->pVarNums[Pk->Id], 1), toLitCond(cnf_2->pVarNums[Pk->Id], 0), toLitCond(miter, 0)};
+  int array5[] = {toLitCond(cnf_1->pVarNums[Pk->Id], 0), toLitCond(cnf_2->pVarNums[Pk->Id], 1), toLitCond(miter, 0)};
+  int array6[] = {toLitCond(cnf_1->pVarNums[Pk->Id], 0), toLitCond(cnf_2->pVarNums[Pk->Id], 0), toLitCond(miter, 1)};
+  int array7[] = {toLitCond(cnf_1->pVarNums[Pk->Id], 1), toLitCond(cnf_2->pVarNums[Pk->Id], 1), toLitCond(miter, 1)};
+  int array8[] = {toLit(miter)};
+  sat_solver_addclause(solver, array4, array4 + sizeof(array4) / sizeof(int));
+  sat_solver_addclause(solver, array5, array5 + sizeof(array5) / sizeof(int));
+  sat_solver_addclause(solver, array6, array6 + sizeof(array6) / sizeof(int));
+  sat_solver_addclause(solver, array7, array7 + sizeof(array7) / sizeof(int));
+  sat_solver_addclause(solver, array8, array8 + sizeof(array8) / sizeof(int));
   // int var_2_i = cnf_2->pVarNums[Pi->Id];
   // int var_2_j = cnf_2->pVarNums[Pj->Id];
+  int status = sat_solver_solve_internal(solver);
+  if (status == 1) {
+    printf("asymmetric\n");
+    // int t = sat_solver_var_value(solver, cnf_1->pVarNums[Pk->Id]);
+    // printf("id: %d ", cnf_1->pVarNums[Pk->Id]);
+    // printf("value: %d\n", t);
+    // int t1 = sat_solver_var_value(solver, cnf_2->pVarNums[Pk->Id]);
+    // printf("id: %d ", cnf_2->pVarNums[Pk->Id]);
+    // printf("value: %d\n", t1);
+    Aig_ManForEachCi(aig_1, Pi, m) {
+      int v = sat_solver_var_value(solver, cnf_1->pVarNums[Pi->Id]);
+      printf("%d", v);
+    // Abc_NtkForEachCi(pNtk, Pi, m) {
+      // printf("cnf12_id: %d ",cnf_2->pVarNums[Pi->Id]);
+      // printf("value: %d\n", v);
+
+      // printf("cnf11_id: %d ",cnf_1->pVarNums[Pi->Id]);
+      // int v1 = sat_solver_var_value(solver, cnf_1->pVarNums[Pi->Id]);
+      // printf("value: %d\n", v1);
+    }
+    printf("\n");
+    Aig_ManForEachCi(aig_1, Pi, m) {
+      int v = sat_solver_var_value(solver, cnf_2->pVarNums[Pi->Id]);
+      printf("%d", v);
+    }
+    printf("\n");
+  }
+  else if (status == -1) {
+    printf("symmetric\n");
+  }
+
+
 
   
 }
