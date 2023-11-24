f8ff49d314a459f7d7346821f62bb92238ab10cf
嚴培勻 <b09901086@zeus.ee.ntu.edu.tw>
2023-11-24 19:18:51 +0800

    lsv_bdd_sym done

--- src/ext-lsv/lsvCmd.cpp
+++ src/ext-lsv/lsvCmd.cpp
@@ -2,17 +2,21 @@
 #include "base/main/main.h"
 #include "base/main/mainInt.h"
 #include "opt/sim/sim.h"
+#include <unordered_map>
 
 static void print_binary(unsigned int number, int bits);
+static int  myStrtol(char* num, int base);
 static int readSimPattern(Abc_Ntk_t * pNtk ,Abc_Obj_t* pFanin, char ** argv);
 static int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv);
 static int Abc_CommandLSVAigSim( Abc_Frame_t * pAbc, int argc, char ** argv );
 static int Abc_CommandLSVBDDSim( Abc_Frame_t * pAbc, int argc, char ** argv );
+static int Abc_CommandLSVSymBDD( Abc_Frame_t * pAbc, int argc, char ** argv );
 
 void init(Abc_Frame_t* pAbc) {
   Cmd_CommandAdd(pAbc, "LSV", "lsv_print_nodes", Lsv_CommandPrintNodes, 0);
   Cmd_CommandAdd( pAbc, "LSV","lsv_sim_aig",Abc_CommandLSVAigSim,0 );
   Cmd_CommandAdd( pAbc, "LSV","lsv_sim_bdd",Abc_CommandLSVBDDSim,0 );
+  Cmd_CommandAdd( pAbc, "LSV","lsv_sym_bdd",Abc_CommandLSVSymBDD,0 );
 }
 
 void destroy(Abc_Frame_t* pAbc) {}
@@ -255,6 +259,204 @@ int Abc_CommandLSVBDDSim( Abc_Frame_t * pAbc, int argc, char ** argv ){
   return 0;
 
 }
+/**Function*************************************************************
+
+  Synopsis    [determine whether an output is symmetric under two given inputs]
+
+  Description [PA2 of LSV]
+
+  SideEffects []
+
+  Usage [lsv sym bdd <output index> <input1 index> <input2 index>]
+  
+  SeeAlso     []
+
+***********************************************************************/
+int Abc_CommandLSVSymBDD( Abc_Frame_t * pAbc, int argc, char ** argv ){
+  Abc_Ntk_t * pNtk = Abc_FrameReadNtk(pAbc);
+  // Abc_Obj_t * pObj1;
+  if(argc != 4){
+      printf("should have only 3 argument!\n");
+      return 0;
+  }
+  if(!Abc_NtkIsBddLogic(pNtk)){
+    printf("Run \"collaspe\" before using this command!\n");
+    return 0;
+  }
+  //output index, input 1 index, input 2 index
+  int oId = myStrtol(argv[1], 10), i1Id = myStrtol(argv[2], 10), i2Id = myStrtol(argv[3], 10);
+  if(oId == -1 || i1Id == -1 || i2Id == -1){
+    printf("parameters should all be positive integer!\n");
+    return 0;
+  }
+  if(i1Id == i2Id){
+    printf("symmetric\n");
+    return 0;
+  }
+  if(oId >= Abc_NtkPoNum(pNtk)){
+    printf("output index should <= %d\n", Abc_NtkPoNum(pNtk) - 1);
+    return 0;
+  }
+  if(i1Id >= Abc_NtkPiNum(pNtk) || i2Id >= Abc_NtkPiNum(pNtk)){
+    printf("input index should <= %d\n", Abc_NtkPiNum(pNtk) - 1);
+    return 0;
+  }
+
+  Abc_Obj_t* pRoot = Abc_ObjFanin0(Abc_NtkCo(pNtk, oId));
+  char* cex1 = new char[Abc_NtkCiNum(pNtk)], *cex2 = new char[Abc_NtkCiNum(pNtk)];
+  // std::unordered_map<int, int> obj2bdd;
+  int* obj2bdd;
+  obj2bdd = new int[Abc_NtkCiNum(pNtk)];
+  for(int i = 0, n = Abc_NtkCiNum(pNtk); i < n; ++i)
+    obj2bdd[i] = -1;
+  int origId1 = i1Id, origId2 = i2Id;
+  char** vNamesIn = (char**) Abc_NodeGetFaninNames(pRoot)->pArray;
+  for(int i = 0, ni = Abc_ObjFaninNum(pRoot); i < ni; ++i){
+    for(int j = 0, nj = Abc_NtkCiNum(pNtk); j < nj; ++j){
+      if(strcmp(Abc_ObjName(Abc_NtkCi(pNtk, j)), vNamesIn[i]) == 0){
+        obj2bdd[j] = i;
+        break;
+      }
+    }
+  }
+  /*
+  printf("logic : \n");
+  for(int i = 0, ni = Abc_NtkCiNum(pNtk); i < ni; ++i){
+    printf("%s, ", Abc_ObjName(Abc_NtkCi(pNtk, i)));
+  }
+  printf("\nbdd : \n");
+  for(int i = 0, ni = Abc_ObjFaninNum(pRoot); i < ni; ++i){
+    printf("%s, ", vNamesIn[i]);
+  }
+  printf("\n");
+  for(int i = 0, n = Abc_NtkCiNum(pNtk); i < n; ++i){
+    if(obj2bdd[i] != -1)
+      printf("obj = %d, bdd = %d\n", i, obj2bdd[i]);
+  }
+  // for(auto it=obj2bdd.begin();it!=obj2bdd.end();it++) {
+  //   printf("obj = %d, bdd = %d \n", it->first, it->second);
+  // }
+  */
+  int first1 = 1, first2 = 1;
+  for(int i = 0, n = Abc_ObjFaninNum(pRoot); i < n; ++i){
+    if(first1 == 1 && strcmp(Abc_ObjName(Abc_NtkCi(pNtk, i1Id)), vNamesIn[i]) == 0){
+      i1Id = i;
+      first1 = 0;
+    }
+    else if(first2 == 1 && strcmp(Abc_ObjName(Abc_NtkCi(pNtk, i2Id)), vNamesIn[i]) == 0){
+      i2Id = i;
+      first2 = 0;
+    }
+  }
+  // printf("first1 = %d, %s; first2 = %d, %s\n", first1, Abc_ObjName(Abc_NtkCi(pNtk, origId1)), first2, Abc_ObjName(Abc_NtkCi(pNtk, origId2)));
+  if(first1 && first2){
+    printf("symmetric\n");
+    return 0;
+  }
+  DdManager * dd = (DdManager *)pNtk->pManFunc;
+  DdNode* ddfnotx1, *ddfx1, *ddfnotx2, *ddfx2, *ddfnotx1x2, *ddfx1notx2;
+  DdNode* ddInput1, *ddInput2;
+  DdNode* ddOutput = (DdNode *)pRoot->pData; Cudd_Ref(ddOutput);
+  ddInput1 = Cudd_bddIthVar(dd, i1Id); Cudd_Ref(ddInput1);
+  ddInput2 = Cudd_bddIthVar(dd, i2Id); Cudd_Ref(ddInput2);
+  int isSymm = -1, type = -1;
+  if(first2 == 1 && first1 == 0){
+    ddfx1    = Cudd_Cofactor(dd, ddOutput, ddInput1); Cudd_Ref(ddfx1);
+    ddfnotx1 = Cudd_Cofactor(dd, ddOutput, Cudd_Not(ddInput1)); Cudd_Ref(ddfnotx1);
+    isSymm = (ddfx1 == ddfnotx1) ? 1 : 0;
+    type = 1;
+    
+  }
+  else if(first1 == 1 && first2 == 0){
+    ddfx2    = Cudd_Cofactor(dd, ddOutput, ddInput2); Cudd_Ref(ddfx2);
+    ddfnotx2 = Cudd_Cofactor(dd, ddOutput, Cudd_Not(ddInput2)); Cudd_Ref(ddfnotx2);
+    isSymm = (ddfx2 == ddfnotx2) ? 1 : 0;
+    type = 2;
+
+    
+  }
+  else{
+    ddfnotx1x2 = Cudd_Cofactor(dd, ddOutput, Cudd_Not(ddInput1)); Cudd_Ref(ddfnotx1x2);
+    ddfnotx1x2 = Cudd_Cofactor(dd, ddfnotx1x2, ddInput2);
+
+    ddfx1notx2 = Cudd_Cofactor(dd, ddOutput, ddInput1); Cudd_Ref(ddfx1notx2);
+    ddfx1notx2 = Cudd_Cofactor(dd, ddfx1notx2, Cudd_Not(ddInput2));
+    isSymm = (ddfnotx1x2 == ddfx1notx2) ? 1 : 0;
+    type = 3;
+
+    
+  }
+  // printf("type = %d\n", type);
+  if(isSymm == -1 || isSymm == 1)
+    printf("symmetric\n");
+  else{
+    printf("asymmetric\n");
+    DdNode* tmp1, *tmp2;
+    // char cex1[Abc_ObjFaninNum(pRoot)], cex2[Abc_ObjFaninNum(pRoot)];
+    for(int i = 0, n = Abc_NtkCiNum(pNtk); i < n; ++i){
+        cex1[i] = '0'; cex2[i] = '0';}
+    if(type == 1){
+      tmp1 = ddfnotx1; tmp2 = ddfx1;
+      // cex1[origId1] = '1'; cex2[origId1] = '0';
+    }
+    else if(type == 2){
+      tmp1 = ddfnotx2; tmp2 = ddfx2;
+      // cex1[origId2] = '1'; cex2[origId2] = '0';
+    }
+    else{
+      tmp1 = ddfnotx1x2; tmp2 = ddfx1notx2;
+    }
+    cex1[origId1] = '0'; cex1[origId2] = '1';
+    cex2[origId1] = '1'; cex2[origId2] = '0';
+    for(int i = 0, ni = Abc_NtkCiNum(pNtk); i < ni; ++i){
+      if(obj2bdd[i] == -1 || (type == 1 && i == origId1) || (type == 2 && i == origId2) || (type == 3 && i == origId1) || (type == 3 && i == origId2))
+        continue;
+      DdNode* curVar = Cudd_bddIthVar(dd, obj2bdd[i]); Cudd_Ref(curVar);
+      if(Cudd_Cofactor(dd, tmp1, curVar) == Cudd_Cofactor(dd, tmp2, curVar)){
+        tmp1 = Cudd_Cofactor(dd, tmp1, Cudd_Not(curVar));
+        tmp2 = Cudd_Cofactor(dd, tmp2, Cudd_Not(curVar));
+        cex1[i] = '0'; cex2[i] = '0';
+      }
+      else{
+        tmp1 = Cudd_Cofactor(dd, tmp1, curVar);
+        tmp2 = Cudd_Cofactor(dd, tmp2, curVar);
+        cex1[i] = '1'; cex2[i] = '1';
+      }
+      Cudd_RecursiveDeref(dd, curVar);
+    }
+    for(int i = 0, ni = Abc_NtkCiNum(pNtk); i < ni; ++i)
+      printf("%c", cex1[i]);
+    printf("\n");
+    for(int i = 0, ni = Abc_NtkCiNum(pNtk); i < ni; ++i)
+      printf("%c", cex2[i]);
+    printf("\n");
+  }
+  switch(type){
+    case 1:
+      Cudd_RecursiveDeref(dd, ddfx1);
+      Cudd_RecursiveDeref(dd, ddfnotx1);
+      break;
+    case 2:
+      Cudd_RecursiveDeref(dd, ddfx2);
+      Cudd_RecursiveDeref(dd, ddfnotx2);
+      break;
+    case 3:
+      Cudd_RecursiveDeref(dd, ddfnotx1x2);
+      Cudd_RecursiveDeref(dd, ddfx1notx2);
+      break;
+    default:
+      return 0;
+  }
+
+  Cudd_RecursiveDeref(dd, ddInput1);
+  Cudd_RecursiveDeref(dd, ddInput2);
+  Cudd_RecursiveDeref(dd, ddOutput);
+  delete []cex1;
+  delete []cex2;
+  delete []obj2bdd;
+  return 0;
+
+}
 
 //====================================================================
 //======================= Helper Function ============================
@@ -277,3 +479,15 @@ int readSimPattern(Abc_Ntk_t * pNtk ,Abc_Obj_t* pFanin, char ** argv){
     }
     return -1;
 }
+
+int myStrtol(char* num, int base){
+    char* endptr;
+    int result = strtol(num, &endptr, base);
+    // Check for conversion errors
+    if (*endptr != '\0' && *endptr != '\n' || result < 0) {
+        return -1;
+    } else {
+        return result;
+    }
+}
+
