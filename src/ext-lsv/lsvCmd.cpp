450ae7c9f934cb8f2f4f2653e61547646f54c656
hhhh1300 <wherechi0912@gmail.com>
2024-10-06 18:12:43 +0800

    fix duplicate bug

--- src/ext-lsv/lsvCmd.cpp
+++ src/ext-lsv/lsvCmd.cpp
@@ -5,6 +5,11 @@
 #include <map>
 #include <vector>
 #include <algorithm>
+#include <iostream>
+#include <set>
+
+using Cut = std::vector<unsigned int>;
+using CutSet = std::set<Cut>;
 
 static int Lsv_CommandPrintNodes(Abc_Frame_t* pAbc, int argc, char** argv);
 static int Lsv_CommandPrintCut(Abc_Frame_t* pAbc, int argc, char** argv);
@@ -66,8 +71,8 @@ usage:
 }
 
 // TODO PA1 Q4
-void Lsv_NtkCrossProduct(const std::map<unsigned int, std::vector<std::vector<unsigned int>>> &mp, 
-                                const std::vector<unsigned int> &fanins, std::vector<std::vector<unsigned int>> &res, const int& k) {
+void Lsv_NtkCrossProduct(const std::map<unsigned int, CutSet> &mp, 
+                                const std::vector<unsigned int> &fanins, CutSet &res, const int& k) {
   // Check if fanins is empty
   if (fanins.empty()) {
     return;  // Nothing to do
@@ -88,103 +93,73 @@ void Lsv_NtkCrossProduct(const std::map<unsigned int, std::vector<std::vector<un
         continue;  // Skip if the fanin is not in the map
     }
 
-    const std::vector<std::vector<unsigned int>> &next_set = mp.at(fanin);
-    std::vector<std::vector<unsigned int>> new_res;
+    const CutSet &next_set = mp.at(fanin);
+    CutSet new_res;
 
-    // Perform cross-product between current result and the next set of vectors
-    for (const auto &current_vec : res) {
-        for (const auto &next_vec : next_set) {
-            // if current cut is > k
-            if (current_vec.size() > k || next_vec.size() > k || current_vec.size() + next_vec.size() > k)
-              continue;
-            std::vector<unsigned int> combined_vec = current_vec;
-            combined_vec.insert(combined_vec.end(), next_vec.begin(), next_vec.end());
-            new_res.push_back(combined_vec);
-        }
+    // Perform cross-product between current result and the next set of cuts
+    for (const auto &current_cut : res) {
+      for (const auto &next_cut : next_set) {
+        Cut combined_cut = current_cut;
+        combined_cut.insert(combined_cut.end(), next_cut.begin(), next_cut.end());
+
+        // Remove duplicates within the cut
+        std::sort(combined_cut.begin(), combined_cut.end());
+        combined_cut.erase(
+            std::unique(combined_cut.begin(), combined_cut.end()),
+            combined_cut.end());
+
+        // Check the size after removing duplicates
+        if (combined_cut.size() > static_cast<size_t>(k))
+          continue;
+
+        new_res.insert(combined_cut);  // Add to the new result set
+      }
     }
 
-    res = new_res;  // Update result
+    res = std::move(new_res);  // Update result
   }
 
-  // remove duplicate
-  for (size_t i = 0; i < res.size(); i++) {
-    std::sort(res[i].begin(), res[i].end());
-    res[i].erase(std::unique(res[i].begin(), res[i].end()), res[i].end());
-  }
 }
 
 void Lsv_NtkPrintCuts(Abc_Ntk_t* pNtk, int k) {
-  std::map<unsigned int, std::vector<std::vector<unsigned int>>> mp;
+  std::map<unsigned int, CutSet> mp;
   Abc_Obj_t* pObj;
   int i;
+
   Abc_NtkForEachPi(pNtk, pObj, i) {
-    mp[Abc_ObjId(pObj)] = std::vector<std::vector<unsigned int>>{std::vector<unsigned int>{Abc_ObjId(pObj)}};
-    printf("%d: %d\n", Abc_ObjId(pObj), Abc_ObjId(pObj));
+    unsigned int obj_id = Abc_ObjId(pObj);
+    mp[obj_id] = {{obj_id}};
+    printf("%d: %d\n", obj_id, obj_id);
   }
+
   Abc_NtkForEachNode(pNtk, pObj, i) {
-    // printf("Object Id = %d, name = %s\n", Abc_ObjId(pObj), Abc_ObjName(pObj));
+    unsigned int obj_id = Abc_ObjId(pObj);
     Abc_Obj_t* pFanin;
     int j;
     std::vector<unsigned int> fanins;
     // For each node, find their fanins
     Abc_ObjForEachFanin(pObj, pFanin, j) {
-      // printf("  Fanin-%d: Id = %d, name = %s\n", j, Abc_ObjId(pFanin),
-      //        Abc_ObjName(pFanin));
       fanins.push_back(Abc_ObjId(pFanin));
-      // if (mp.find(Abc_ObjId(pFanin)) == mp.end())
-      //   mp[Abc_ObjId(pFanin)] = std::vector<std::vector<unsigned int>>{std::vector<unsigned int>{Abc_ObjId(pFanin)}};
     }
 
-    // the pObj itself is a cut too
-    std::vector<std::vector<unsigned int>> res;
-    // mp[Abc_ObjId(pObj)] = res;
-    // find cuts with respect to the pObj
+    CutSet res;
     Lsv_NtkCrossProduct(mp, fanins, res, k);
-    res.insert(std::begin(res), std::vector<unsigned int>{Abc_ObjId(pObj)});
-    for (int i = 0; i < res.size(); i++) {
-      // if the size of cut > k, ignore
-      if (res[i].size() > k)
-        continue;
-      printf("%d: ", Abc_ObjId(pObj));
-      for (int j = 0; j < res[i].size(); j++) {
-        printf("%d", res[i][j]);
-        if (j == res[i].size()-1)
-          printf("\n");
-        else
-          printf(" ");
-      }
-    }
-    mp[Abc_ObjId(pObj)] = res;
-  }
-  // Abc_NtkForEachPo(pNtk, pObj, i) {
-  //   // printf("Object Id = %d, name = %s\n", Abc_ObjId(pObj), Abc_ObjName(pObj));
-  //   Abc_Obj_t* pFanin;
-  //   int j;
-  //   std::vector<unsigned int> fanins;
-  //   // For each node, find their fanins
-  //   Abc_ObjForEachFanin(pObj, pFanin, j) {
-  //     fanins.push_back(Abc_ObjId(pFanin));
-  //   }
 
-  //   // the pObj itself is a cut too
-  //   std::vector<std::vector<unsigned int>> res;
-  //   // find cuts with respect to the pObj
-  //   Lsv_NtkCrossProduct(mp, fanins, res, k);
-  //   res.insert(std::begin(res), std::vector<unsigned int>{Abc_ObjId(pObj)});
-  //   for (int i = 0; i < res.size(); i++) {
-  //     // if the size of cut > k, ignore
-  //     if (res[i].size() > k)
-  //       continue;
-  //     printf("%d: ", Abc_ObjId(pObj));
-  //     for (int j = 0; j < res[i].size(); j++) {
-  //       printf("%d", res[i][j]);
-  //       if (j == res[i].size()-1)
-  //         printf("\n");
-  //       else
-  //         printf(" ");
-  //     }
-  //   }
-  // }
+    // Include the node itself as a cut
+    res.insert({obj_id});
+
+    // Store the cuts for future use
+    mp[obj_id] = res;
+    
+    // Output the cuts
+    for (const auto &cut : res) {
+      printf("%d:", obj_id);
+      for (size_t idx = 0; idx < cut.size(); ++idx) {
+        printf(" %d", cut[idx]);
+      }
+      printf("\n");
+    }
+  }
 }
 
 int Lsv_CommandPrintCut(Abc_Frame_t* pAbc, int argc, char** argv) {
